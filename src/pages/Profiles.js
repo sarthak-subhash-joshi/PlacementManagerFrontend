9a4e6b27758ffe768f7c0d47de86e132c6599c14
sarthak-subhash-joshi <sarthakjoshi3011@gmail.com>
2024-04-03 19:53:27 +0530

    Adding feature of moving to top on changing page count in pagination

--- src/pages/Profiles.js
+++ src/pages/Profiles.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import "../styles/pages/Profiles.css";
 import axios from "axios";
 import { NavLink } from "react-router-dom";
@@ -9,25 +9,33 @@ import PacmanLoader from "react-spinners/FadeLoader";
 import { BASE_URL } from "../Helper";
 import BulkStudentDataUpload from "../components/BulkStudentDataUpload";
 
-
 const Profiles = () => {
   const { students, dispatch } = useStudentRecordContext();
-  const[pageSize,setPageSize]=useState(10);
+  const [pageSize, setPageSize] = useState(10);
 
   const [currentPage, setCurrentPage] = useState(1);
   const [totalPages, setTotalPages] = useState(0);
   const [searchTerm, setSearchTerm] = useState("");
   const [filteredStudents, setFilteredStudents] = useState([]);
 
-  const[placementData,setPlacementData]=useState([])
+  const [placementData, setPlacementData] = useState([]);
+  const tableRef = useRef(null);
 
   useEffect(() => {
     fetchRecordCount();
-  }, [currentPage, searchTerm, dispatch,pageSize]);
+  }, [currentPage, searchTerm, dispatch, pageSize]);
+
+  useEffect(() => {
+    if (tableRef.current) {
+      tableRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+  }, [currentPage]);
 
   const fetchRecordCount = async () => {
     try {
-      const response = await axios.get(`${BASE_URL}/api/student_record?q=${searchTerm}`);
+      const response = await axios.get(
+        `${BASE_URL}/api/student_record?q=${searchTerm}`
+      );
       const totalRecords = response.data.length;
       setTotalPages(Math.ceil(totalRecords / pageSize));
 
@@ -36,9 +44,10 @@ const Profiles = () => {
       const end = start + pageSize;
       setFilteredStudents(response.data.slice(start, end));
 
-      const placementResponse = await axios.get(`${BASE_URL}/api/placement_record`);
+      const placementResponse = await axios.get(
+        `${BASE_URL}/api/placement_record`
+      );
       setPlacementData(placementResponse.data);
-
     } catch (error) {
       console.error("Error fetching record count:", error);
     }
@@ -71,7 +80,8 @@ const Profiles = () => {
     <>
       <div className="search-bar-container">
         <label className="search-label" htmlFor="searchTerm">
-          Search by First Name, Middle Name, Last Name, Email, PRN, or Mobile Number
+          Search by First Name, Middle Name, Last Name, Email, PRN, or Mobile
+          Number
         </label>
         <input
           className="search-input"
@@ -89,139 +99,153 @@ const Profiles = () => {
       </div>
 
       <div className="container-home-top ">
-          <NavLink to="/add_student">
-            <button id="addUserBtn" type="button" className="btn btn-success">
-              <i className="fa-solid fa-plus"></i> Add Student 
-            </button>
-          </NavLink>
-         <BulkStudentDataUpload/>
-
+        <NavLink to="/add_student">
+          <button id="addUserBtn" type="button" className="btn btn-success">
+            <i className="fa-solid fa-plus"></i> Add Student
+          </button>
+        </NavLink>
+        <BulkStudentDataUpload />
       </div>
 
-     
-    
-      <div  className="subject-container ">
+      <div className="subject-container" ref={tableRef}>
         <h4 className="subject-heading">Student's Data (Master Data)</h4>
         {filteredStudents.length === 0 ? (
           <>
-            {searchTerm==="" ? (<PacmanLoader color="#36d7b7" />) : (<div style={{color:'#00337C'}}>No records found </div>)}
+            {searchTerm === "" ? (
+              <PacmanLoader color="#36d7b7" />
+            ) : (
+              <div style={{ color: "#00337C" }}>No records found </div>
+            )}
           </>
         ) : (
           <>
-          <div style={{ overflowX: "auto" }}>
-          <table  className="table table-striped">
-          <thead>
-            <tr>
-              <th className="row-border">#</th>
-              <th className="row-border">Name</th>
-              <th className="row-border">Email</th>
-              <th className="row-border">PRN</th>
-              <th className="row-border">Mobile Number</th>
-              <th className="row-border">Course</th>
-              <th className="row-border">Branch</th>
-              <th className="row-border">Batch</th>
-              <th className="row-border">College</th>
-              <th className="row-border">Status</th>
-              <th className="row-border">
-                  Preview
-              </th>
-              <th  className="row-border">
-                  Delete
-              </th>
-              <th  className="row-border">
-                  Edit
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            {filteredStudents.map((student, index) => (
-              <tr key={student._id}>
-                <td className="row-border" scope="row">
-                  {index + 1 + pageSize * (currentPage - 1)}
-                </td>
-                <td className="row-border">{`${student.firstName} ${student.middleName} ${student.lastName}`}</td>
-                <td className="row-border">{student.email}</td>
-                <td className="row-border">{student.universityPRN}</td>
-                <td className="row-border">{student.mobileNo}</td>
-                <td className="row-border">{student.course}</td>
-                <td className="row-border">{student.branch}</td>
-                <td className="row-border">{student.batch}</td>
-                <td className="row-border">{student.collegeName}</td>
-                <td className="row-border" style={{color:"#008DDA",fontWeight:'bolder'}}>{placementData.find(entry => entry.studentId === student._id) !== undefined  ? 'Placed' : 'Unplaced'}</td>
-                <td  className="row-border btn-container">
-                  <NavLink to={`/profile/${student._id}`}>  <i className="fa-regular fa-eye preview-icon"></i></NavLink>
-                </td>
-                <td id="BtnContainer" className="row-border btn-container">
-                    <i className="fa-solid fa-trash delete-icon" onClick={() => handleDelete(student._id)}></i>
-                </td>
-                <td id="BtnContainer" className="row-border btn-container">
-                <NavLink to={`/update_student_record/${student._id}`}> <i class="fa-solid fa-pen-to-square update-icon"></i></NavLink>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-          </div>
-       
+            <div style={{ overflowX: "auto" }}>
+              <table className="table table-striped">
+                <thead>
+                  <tr>
+                    <th className="row-border">#</th>
+                    <th className="row-border">Name</th>
+                    <th className="row-border">Email</th>
+                    <th className="row-border">PRN</th>
+                    <th className="row-border">Mobile Number</th>
+                    <th className="row-border">Course</th>
+                    <th className="row-border">Branch</th>
+                    <th className="row-border">Batch</th>
+                    <th className="row-border">College</th>
+                    <th className="row-border">Status</th>
+                    <th className="row-border">Preview</th>
+                    <th className="row-border">Delete</th>
+                    <th className="row-border">Edit</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {filteredStudents.map((student, index) => (
+                    <tr key={student._id}>
+                      <td className="row-border" scope="row">
+                        {index + 1 + pageSize * (currentPage - 1)}
+                      </td>
+                      <td className="row-border">{`${student.firstName} ${student.middleName} ${student.lastName}`}</td>
+                      <td className="row-border">{student.email}</td>
+                      <td className="row-border">{student.universityPRN}</td>
+                      <td className="row-border">{student.mobileNo}</td>
+                      <td className="row-border">{student.course}</td>
+                      <td className="row-border">{student.branch}</td>
+                      <td className="row-border">{student.batch}</td>
+                      <td className="row-border">{student.collegeName}</td>
+                      <td
+                        className="row-border"
+                        style={{ color: "#008DDA", fontWeight: "bolder" }}
+                      >
+                        {placementData.find(
+                          (entry) => entry.studentId === student._id
+                        ) !== undefined
+                          ? "Placed"
+                          : "Unplaced"}
+                      </td>
+                      <td className="row-border btn-container">
+                        <NavLink to={`/profile/${student._id}`}>
+                          <i className="fa-regular fa-eye preview-icon"></i>
+                        </NavLink>
+                      </td>
+                      <td id="BtnContainer" className="row-border btn-container">
+                        <i
+                          className="fa-solid fa-trash delete-icon"
+                          onClick={() => handleDelete(student._id)}
+                        ></i>
+                      </td>
+                      <td id="BtnContainer" className="row-border btn-container">
+                        <NavLink to={`/update_student_record/${student._id}`}>
+                          {" "}
+                          <i className="fa-solid fa-pen-to-square update-icon"></i>
+                        </NavLink>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
 
-          <div>
-        <select style={{width:'70px'}} value={pageSize} onChange={(e) => setPageSize(parseInt(e.target.value))}>
-  <option value="" disabled>
-  Rows Per Page
-  </option>
-  <option value={5}>5</option> {/* Set value attribute to a number */}
-  <option value={10}>10</option> {/* Set value attribute to a number */}
-  <option value={20}>20</option> {/* Set value attribute to a number */}
-</select>
-        </div>
-        <div className="pagination-container">
+            <div>
+              <select
+                style={{ width: "70px" }}
+                value={pageSize}
+                onChange={(e) => setPageSize(parseInt(e.target.value))}
+              >
+                <option value="" disabled>
+                  Rows Per Page
+                </option>
+                <option value={5}>5</option> {/* Set value attribute to a number */}
+                <option value={10}>10</option>{" "}
+                {/* Set value attribute to a number */}
+                <option value={20}>20</option>{" "}
+                {/* Set value attribute to a number */}
+              </select>
+            </div>
+            <div className="pagination-container">
+              {currentPage !== 1 && (
+                <>
+                  <button
+                    className="pagination-btn"
+                    onClick={() => handlePageChange(1)}
+                  >
+                    <i className="fa-solid fa-backward fa-lg"></i>
+                  </button>
+                  <button
+                    className="pagination-btn"
+                    onClick={() =>
+                      currentPage > 1 && handlePageChange(currentPage - 1)
+                    }
+                  >
+                    <i className="fa-solid fa-caret-left fa-lg"></i>
+                  </button>
+                </>
+              )}
 
-        {currentPage !== 1 && (
-    <>
-    <button className="pagination-btn" onClick={() => handlePageChange(1)}>
-          <i className="fa-solid fa-backward fa-lg"></i>
-        </button>
-        <button
-          className="pagination-btn"
-          onClick={() => currentPage > 1 && handlePageChange(currentPage - 1)}
-        >
-          <i className="fa-solid fa-caret-left fa-lg"></i>
-        </button>
-    </>
-  )}
-   
-       
-        {currentPage} of {totalPages}
+              {currentPage} of {totalPages}
 
-        {currentPage !== totalPages && (
-    <>
-    <button
-          className="pagination-btn"
-          onClick={() =>
-            currentPage < totalPages && handlePageChange(currentPage + 1)
-          }
-        >
-          <i className="fa-solid fa-caret-right fa-lg"></i>
-        </button>
-        <button
-          onClick={() => handlePageChange(totalPages)}
-          className="pagination-btn"
-        >
-          <i className="fa-solid fa-forward fa-lg"></i>
-        </button>
-    </>
-  )}
-      
-      </div>
-
-
-</>
+              {currentPage !== totalPages && (
+                <>
+                  <button
+                    className="pagination-btn"
+                    onClick={() =>
+                      currentPage < totalPages &&
+                      handlePageChange(currentPage + 1)
+                    }
+                  >
+                    <i className="fa-solid fa-caret-right fa-lg"></i>
+                  </button>
+                  <button
+                    onClick={() => handlePageChange(totalPages)}
+                    className="pagination-btn"
+                  >
+                    <i className="fa-solid fa-forward fa-lg"></i>
+                  </button>
+                </>
+              )}
+            </div>
+          </>
         )}
       </div>
-       
-
-      
-    
 
       <ToastContainer
         position="top-right"
